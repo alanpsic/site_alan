d953ff0880299ede6701346a521fefab7e75d36e
Alexsandro Duarte <pocmusic321@gmail.com>
2025-10-08 18:30:21 -0300

    Finalizando página básica

--- src/pages/artigo/index.tsx
+++ src/pages/artigo/index.tsx
@@ -5,6 +5,8 @@ import style from "./style.module.css";
 import { Link } from "react-router-dom";
 
 import primeiroArtigoImg from "../../images/Artigos/SaudeMentalDetail.png";
+import segundoArtigoImg from "../../images/Artigos/CaminhadaImg.png";
+import terceiroArtigoImg from "../../images/Artigos/SaudeMentalDetail.png";
 
 interface ArtigoData
 {
@@ -25,14 +27,14 @@ export const segundoArtigo = {
     tema: "Caminha e Cêrebro",
     texto: ["Em meio à correria do dia a dia, a caminhada parece simples demais para ser poderosa. Mas é justamente nessa simplicidade que mora sua força. Cada passo ativa não apenas músculos, mas também circuitos cerebrais que influenciam nosso humor, memória e capacidade de concentração.", "o caminhar, o cérebro recebe uma dose natural de endorfinas, reduzindo o estresse e promovendo bem-estar. A circulação sanguínea aumenta, levando mais oxigênio às células cerebrais, o que favorece a clareza mental e a criatividade. Não é por acaso que grandes pensadores, como Nietzsche e Darwin, faziam longas caminhadas para estimular ideias.", "Mais do que exercício físico, caminhar é um ritual de reconexão. É quando o barulho externo se silencia e o cérebro encontra espaço para reorganizar pensamentos, processar emoções e até resolver problemas que pareciam insolúveis.", "Caminhar é gratuito, acessível e profundamente transformador. É o tipo de terapia que não exige divã — apenas disposição para sair e deixar o corpo guiar a mente."],
     texto_simplificado: "Em meio à correria do dia a dia, a caminhada parece simples demais para ser poderosa. Mas é justamente nessa simplicidade que mora sua força. Cada passo ativa não apenas músculos, mas também circuitos cerebrais que influenciam nosso humor, memória e capacidade de concentração.",
-    imagem: primeiroArtigoImg
+    imagem: segundoArtigoImg
 };
 
 export const terceiroArtigo = {
     tema: "Saude Mental",
     texto: ["A saúde mental é o alicerce silencioso da nossa existência. Ela não se resume à ausência de doenças, mas à capacidade de lidar com emoções, enfrentar desafios."],
     texto_simplificado: "A saúde mental é o alicerce silencioso da nossa existência. Ela não se resume à ausência de doenças, mas à capacidade de lidar com emoções, enfrentar desafios.",
-    imagem: primeiroArtigoImg
+    imagem: terceiroArtigoImg
 }
 
 export function Artigo()
